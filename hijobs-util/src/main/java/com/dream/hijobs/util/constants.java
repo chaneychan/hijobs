293aaade6ade47037a63ce99933932dda229e4bb
chaney <chaney.chan.sh@gmail.com>
2015-01-05 10:06:18 +0800

    +

--- hijobs-util/src/main/java/com/dream/hijobs/util/constants.java
+++ hijobs-util/src/main/java/com/dream/hijobs/util/constants.java
@@ -3,7 +3,7 @@ package com.dream.hijobs.util;
 public class constants {
     /**
      * token 代表访问者身份
-     * 前段的filed
+     * 前端的filed
      */
     public static final String TOKEN_CMD = "tk";
     
